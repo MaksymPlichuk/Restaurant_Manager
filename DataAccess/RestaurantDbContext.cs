d15b177585b2a46375f829e8cf4081225e28526c
Mark <markmyskovets9@gmail.com>
2025-10-30 20:14:00 +0200

    OnConfiguring

--- DataAccess/RestaurantDbContext.cs
+++ DataAccess/RestaurantDbContext.cs
@@ -12,6 +12,14 @@ namespace DataAccess
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"workstation id=RestaurantDatabase.mssql.somee.com;
+                                        packet size=4096;user id=OlegVinnuk322_SQLLogin_1;
+                                        pwd=gnwrtyu3qb;
+                                        data source=RestaurantDatabase.mssql.somee.com;
+                                        persist security info=False;
+                                        initial catalog=RestaurantDatabase;
+                                        TrustServerCertificate=True
+");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
