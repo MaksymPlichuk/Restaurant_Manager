e3ed87a326a55d0d06ea7f1a596d5c66606c1218
MKA <MKA@A21PC16>
2025-10-31 16:55:45 +0200

    +Relations

--- DataAccess/Helpers/DbInitializer.cs
+++ DataAccess/Helpers/DbInitializer.cs
@@ -17,5 +17,34 @@ namespace DataAccess.Helpers
             //    new Category { Id = 1, Name = "test", Products = }
             //    );
         }
+        public static void SeedBooking(this ModelBuilder modelBuilder)
+        {
+            
+
+
+        }
+        public static void SeedOrder(this ModelBuilder modelBuilder)
+        {
+            
+
+
+        }
+        public static void SeedProduct(this ModelBuilder modelBuilder)
+        {
+            
+
+
+        }
+        public static void SeedReview(this ModelBuilder modelBuilder)
+        {
+            
+
+
+        }
+        public static void SeedUser(this ModelBuilder modelBuilder)
+        {
+            
+
+        }
     }
 }
