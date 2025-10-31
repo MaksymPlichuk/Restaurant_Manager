5072b6babbfd0474d46d8ead8c5e85057766454f
Maksym <pilipm449@gmail.com>
2025-10-31 12:09:52 +0200

    Added DbSets, base for Seeder

--- DataAccess/Entities/Order.cs
+++ DataAccess/Entities/Order.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Entities
 {
-    internal class Order
+    public class Order
     {
         public int Id { get; set; }
         public User User { get; set; }
--- DataAccess/RestaurantDbContext.cs
+++ DataAccess/RestaurantDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataAccess.Entities;
+using DataAccess.Helpers;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,11 +21,24 @@ namespace DataAccess
                                         persist security info=False;
                                         initial catalog=RestaurantDatabase;
                                         TrustServerCertificate=True
-");
+            ");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            //зв'язки
+            //modelBuilder.Entity<Booking>
+
+
+            //modelBuilder.SeedCategories();
         }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+}
     }
 }
