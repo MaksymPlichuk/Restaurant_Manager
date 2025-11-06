a68b80c921e6341c1c3a2ecc2ba81da77cd8a561
Maksym <pilipm449@gmail.com>
2025-11-06 19:00:42 +0200

    Added client menu and add to cart btn

---
+++ DataAccess/Migrations/20251106145947_FixedDates.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "BookingDate",
+                value: new DateTime(2025, 11, 7, 19, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "BookingDate",
+                value: new DateTime(2025, 12, 11, 20, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2025, 11, 6, 15, 3, 55, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2025, 11, 6, 18, 43, 24, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2025, 11, 6, 11, 18, 44, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "BookingDate",
+                value: new DateTime(2025, 11, 6, 19, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "BookingDate",
+                value: new DateTime(2025, 11, 7, 20, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2025, 11, 5, 15, 3, 12, 439, DateTimeKind.Local).AddTicks(5482));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2025, 11, 5, 16, 3, 12, 439, DateTimeKind.Local).AddTicks(6042));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2025, 11, 5, 16, 33, 12, 439, DateTimeKind.Local).AddTicks(6055));
+        }
+    }
+}
